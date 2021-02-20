c949c3468a483630ffad3637a3f8324c141dfeea
tmujje <tmujje@gmail.com>
2021-02-19 22:17:19 -0500

    Add config

--- IRmqClient.cs
+++ IRmqClient.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using EasyNetQ;
+using EasyNetQ.Topology;
 
 namespace RmqClient
 {
     public interface IRmqClient
     {
-
+        Task SendRequests(IAdvancedBus advancedBus, IQueue responseQueue, IExchange exchange);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,21 +1,49 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
+﻿using System;
 using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
-namespace RmqMessenger
+namespace RmqClient
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
 
+            Log.Logger.Information($"Application Starting");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<IRmqClient, RmqClient>();
+                })
+                .UseSerilog()
+                .Build();
+        }
+        
         static void BuildConfig(IConfigurationBuilder builder)
         {
             builder.SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}");
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",true)
+                .AddEnvironmentVariables();
+        }
+    }
+
+    public class Startup
+    {
+        public void Run()
+        {
+
         }
     }
 }
